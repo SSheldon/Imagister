cd654e7b0a8f26785f0b21d462b8ee1d6a619355
Steven Sheldon <steven.sheldon@live.com>
2012-04-01 23:24:03 +0000

    Added stubbed-out ManipulableImage class.

---
+++ Imagister/ManipulableImage.cs
@@ -0,0 +1,33 @@
+﻿namespace Imagister
+{
+	public class ManipulableImage
+	{
+		public ManipulableImage(int height, int width, int[] pixels)
+		{
+		}
+
+		public void FlipVertical()
+		{
+		}
+
+		public void FlipHorizontal()
+		{
+		}
+
+		public void RotateDown()
+		{
+		}
+
+		public void RotateRight(int[] pixels)
+		{
+		}
+
+		public void RotateLeft(int[] pixels)
+		{
+		}
+
+		public void Resize(int height, int width, int[] pixels)
+		{
+		}
+	}
+}
