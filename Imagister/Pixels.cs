7984d2cf20162c8c40e7ab378d8c7dfa02aee0b2
Steven Sheldon <steven.sheldon@live.com>
2012-04-05 19:45:21 +0000

    Added IPixels interface in Pixels.cs file.

---
+++ Imagister/Pixels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Imagister
+{
+	/// <summary>
+	/// Represents a rectangle of pixels.
+	/// </summary>
+	public interface IPixels
+	{
+		/// <summary>
+		/// Gets the height of this IPixels.
+		/// </summary>
+		int Height
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets the width of this IPixels.
+		/// </summary>
+		int Width
+		{
+			get;
+		}
+
+		/// <summary>
+		/// Gets or sets the premultiplied ARGB pixel
+		/// at the specified row and column indices in this IPixels.
+		/// </summary>
+		/// <param name="row">The row index of the pixel.</param>
+		/// <param name="col">The column index of the pixel.</param>
+		int this[int row, int col]
+		{
+			get;
+			set;
+		}
+	}
+}
