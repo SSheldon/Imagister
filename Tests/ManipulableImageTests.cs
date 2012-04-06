fb854ab21fc9c9e4d39fc775b1ecfb49e7a5a9fd
Steven Sheldon <steven.sheldon@live.com>
2012-04-06 11:21:59 +0000

    Added tests for rotations and flips.

--- Tests/ManipulableImageTests.cs
+++ Tests/ManipulableImageTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Drawing;
 using System.Linq;
 using System.Reflection;
@@ -48,6 +49,50 @@ namespace Imagister.Tests
 			Assert.IsNotNull(img);
 			ManipulableImage expected = LoadManipulableImage("3.bmp");
 			Assert.IsTrue(SameData(img, expected));
+		}
+
+		/// <summary>
+		/// Does an action and then compares this Test's image
+		/// with the image with the specified filename.
+		/// </summary>
+		/// <param name="action">The Action to do.</param>
+		/// <param name="filename">The name of the image
+		/// to compare to after the action is done.</param>
+		private void DoAndCompare(Action action, string filename)
+		{
+			action.Invoke();
+			ManipulableImage expected = LoadManipulableImage(filename);
+			Assert.IsTrue(SameData(img, expected));
+		}
+
+		[TestMethod]
+		public void TestFlipVertical()
+		{
+			DoAndCompare(() => img.FlipVertical(), "3_flip-v.bmp");
+		}
+
+		[TestMethod]
+		public void TestFlipHorizontal()
+		{
+			DoAndCompare(() => img.FlipHorizontal(), "3_flip-h.bmp");
+		}
+
+		[TestMethod]
+		public void TestRotateDown()
+		{
+			DoAndCompare(() => img.RotateDown(), "3_rot-d.bmp");
+		}
+
+		[TestMethod]
+		public void TestRotateRight()
+		{
+			DoAndCompare(() => img.RotateRight(), "3_rot-r.bmp");
+		}
+
+		[TestMethod]
+		public void TestRotateLeft()
+		{
+			DoAndCompare(() => img.RotateLeft(), "3_rot-l.bmp");
 		}
 	}
 }
