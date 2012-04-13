0ed6e09db48d9b567308170dc3f49c43c92629a8
Steven Sheldon <steven.sheldon@live.com>
2012-04-13 02:56:36 +0000

    Added the Pixel struct.

---
+++ Imagister/Pixel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Imagister
+{
+	/// <summary>
+	/// Represents a premultiplied ARGB pixel.
+	/// </summary>
+	[StructLayout(LayoutKind.Explicit)]
+	public struct Pixel
+	{
+		/// <summary>
+		/// The int representation of this Pixel.
+		/// </summary>
+		[FieldOffset(0)]
+		public int ARGB;
+
+		[FieldOffset(0)]
+		private byte b;
+		[FieldOffset(1)]
+		private byte g;
+		[FieldOffset(2)]
+		private byte r;
+
+		/// <summary>The red component of this Pixel.</summary>
+		public int R
+		{
+			get { return r; }
+			set { r = (byte)Math.Max(0, Math.Min(255, value)); }
+		}
+		/// <summary>The green component of this Pixel.</summary>
+		public int G
+		{
+			get { return g; }
+			set { g = (byte)Math.Max(0, Math.Min(255, value)); }
+		}
+		/// <summary>The blue component of this Pixel.</summary>
+		public int B
+		{
+			get { return b; }
+			set { b = (byte)Math.Max(0, Math.Min(255, value)); }
+		}
+
+		/// <summary>
+		/// Constructs a Pixel.
+		/// </summary>
+		/// <param name="argb">The argb value for the Pixel.</param>
+		public Pixel(int argb)
+		{
+			r = b = g = 0;
+			ARGB = argb;
+		}
+
+		/// <summary>
+		/// Adds another Pixel to this Pixel.
+		/// </summary>
+		/// <param name="other">The Pixel to be added.</param>
+		public void Add(Pixel other)
+		{
+			R += other.R;
+			G += other.G;
+			B += other.B;
+		}
+
+		/// <summary>
+		/// Subtracts another Pixel from this Pixel.
+		/// </summary>
+		/// <param name="other">The Pixel to be subtracted.</param>
+		public void Subtract(Pixel other)
+		{
+			R -= other.R;
+			G -= other.G;
+			B -= other.B;
+		}
+	}
+}
