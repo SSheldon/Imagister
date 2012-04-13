ad8716e8313bdab8734ce2a015d9813e697ac4f5
Steven Sheldon <steven.sheldon@live.com>
2012-04-13 15:45:54 +0000

    Added delegate for Shaders without coordinate data.

--- Imagister/Shader.cs
+++ Imagister/Shader.cs
@@ -6,10 +6,17 @@ namespace Imagister
 	/// A Shader is a method that shades a pixel.
 	/// </summary>
 	/// <param name="pixel">The premultiplied ARGB color of the pixel.</param>
+	/// <returns>The premultiplied ARGB color after shading.</returns>
+	public delegate int Shader(int pixel);
+
+	/// <summary>
+	/// A CoordShader is a method that shades a pixel based on its coordinates.
+	/// </summary>
+	/// <param name="pixel">The premultiplied ARGB color of the pixel.</param>
 	/// <param name="row">The row index of the pixel.</param>
 	/// <param name="col">The column index of the pixel.</param>
 	/// <returns>The premultiplied ARGB color after shading.</returns>
-	public delegate int Shader(int pixel, int row, int col);
+	public delegate int CoordShader(int pixel, int row, int col);
 
 	/// <summary>
 	/// Provides Shaders and methods for working with them.
@@ -22,6 +29,22 @@ namespace Imagister
 		/// <param name="pixels">The IPixels to shade.</param>
 		/// <param name="shader">The Shader to apply to the pixels.</param>
 		public static void Apply(IPixels pixels, Shader shader)
+		{
+			for (int row = 0; row < pixels.Height; row++)
+			{
+				for (int col = 0; col < pixels.Width; col++)
+				{
+					pixels[row, col] = shader(pixels[row, col]);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Applies a Shader to the IPixels.
+		/// </summary>
+		/// <param name="pixels">The IPixels to shade.</param>
+		/// <param name="shader">The CoordShader to apply to the pixels.</param>
+		public static void Apply(IPixels pixels, CoordShader shader)
 		{
 			for (int row = 0; row < pixels.Height; row++)
 			{
