ef862e1445147cb02da17c9896b766889ac1bd87
Steven Sheldon <steven.sheldon@live.com>
2012-04-13 02:58:48 +0000

    Added Ditherer class.

--- Imagister/Posterization.cs
+++ Imagister/Posterization.cs
@@ -44,4 +44,88 @@ namespace Imagister
 			return (a << 24) | (r << 16) | (g << 8) | b;
 		}
 	}
+
+	public class Ditherer
+	{
+		private Vector3[,] errs;
+		private int depth;
+
+		public Ditherer(int depth, int width, int height)
+		{
+			this.depth = depth;
+			this.errs = new Vector3[height, width];
+		}
+
+		public Ditherer(int depth, IPixels pixels)
+			: this(depth, pixels.Width, pixels.Height)
+		{ }
+
+		private void AddErrors(Vector3 diff, int row, int col)
+		{
+			bool lastCol = col + 1 >= errs.GetLength(1);
+			if (!lastCol)
+			{
+				Vector3 d = diff;
+				d.Multiply(0.4375f);
+				errs[row, col + 1].Add(d);
+			}
+			bool lastRow = row + 1 >= errs.GetLength(0);
+			if (!lastRow && col != 0)
+			{
+				Vector3 d = diff;
+				d.Multiply(0.1875f);
+				errs[row + 1, col - 1].Add(d);
+			}
+			if (!lastRow)
+			{
+				Vector3 d = diff;
+				d.Multiply(0.3125f);
+				errs[row + 1, col].Add(d);
+			}
+			if (!lastRow && !lastCol)
+			{
+				Vector3 d = diff;
+				d.Multiply(0.0625f);
+				errs[row + 1, col + 1].Add(d);
+			}
+		}
+
+		public int Dither(int argb, int row, int col)
+		{
+			int a = (argb >> 24) & 0xFF;
+			int r = (argb >> 16) & 0xFF;
+			int g = (argb >> 8) & 0xFF;
+			int b = (argb) & 0xFF;
+
+			Vector3 res = new Vector3(r, g, b);
+			res.Add(errs[row, col]);
+			res = Posterizer.Posterize(res, depth);
+
+			Vector3 diff = new Vector3(r, g, b);
+			diff.Subtract(res);
+			AddErrors(diff, row, col);
+
+			r = (int)res.X;
+			g = (int)res.Y;
+			b = (int)res.Z;
+			return (a << 24) | (r << 16) | (g << 8) | b;
+		}
+
+		public void Dither(IPixels pixels)
+		{
+			for (int row = 0; row < pixels.Height; row++)
+			{
+				for (int col = 0; col < pixels.Width; col++)
+				{
+					pixels[row, col] = Dither(pixels[row, col], row, col);
+				}
+			}
+		}
+
+		public static void Dither(IPixels pixels, int depth)
+		{
+			Ditherer ditherer = new Ditherer(depth, pixels);
+			ditherer.Dither(pixels);
+		}
+	}
 }
