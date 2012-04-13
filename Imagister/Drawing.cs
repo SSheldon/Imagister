e7640cdbb28a9013d8b5b0a6c260e46ebcd2b438
Steven Sheldon <steven.sheldon@live.com>
2012-04-13 17:20:45 +0000

    Added Drawing class and FillRect method.

---
+++ Imagister/Drawing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Imagister
+{
+	/// <summary>
+	/// Provides methods for drawing shapes on IPixels.
+	/// </summary>
+	public static class Drawing
+	{
+		/// <summary>
+		/// Draws a filled rectangle.
+		/// </summary>
+		/// <param name="pixels">The IPixels to draw on.</param>
+		/// <param name="rect">The rectangle to draw.</param>
+		/// <param name="rgb">The color to use.</param>
+		public static void FillRect(IPixels pixels, PixRect rect, int rgb)
+		{
+			Selection selection = new Selection(pixels, rect);
+			Shaders.Apply(pixels, Colors.GetSolidShader(rgb));
+		}
+	}
+}
