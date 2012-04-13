9972bff633bfb8ab382b428386a631212dc93df9
Steven Sheldon <steven.sheldon@live.com>
2012-04-13 17:21:01 +0000

    Added Bresenham line algorithm.

--- Imagister/Drawing.cs
+++ Imagister/Drawing.cs
@@ -8,6 +8,40 @@ namespace Imagister
 	public static class Drawing
 	{
 		/// <summary>
+		/// Draws a line on the IPixels between two points.
+		/// </summary>
+		/// <param name="pixels">The IPixels to draw on.</param>
+		/// <param name="rgb">The color of the line.</param>
+		public static void DrawLine(IPixels pixels,
+			int x0, int y0, int x1, int y1, int rgb)
+		{
+			rgb = rgb & 0xFFFFFF;
+			//Bresenham line algorithm implementation
+			int sx = (x0 < x1 ? 1 : -1), sy = (y0 < y1 ? 1 : -1);
+			int dx = Math.Abs(x1 - x0), dy = Math.Abs(y1 - y0);
+			int err = dx - dy;
+			while (true)
+			{
+				//set pixel
+				pixels[y0, x0] = pixels[y0, x0] & (0xFF << 24) | rgb;
+				//check for break
+				if (x0 == x1 && y0 == y1) break;
+				//advance x0 and y0
+				int err2 = 2 * err;
+				if (err2 > -dy)
+				{
+					err -= dy;
+					x0 += sx;
+				}
+				if (err2 < dx)
+				{
+					err += dx;
+					y0 += sy;
+				}
+			}
+		}
+
+		/// <summary>
 		/// Draws a filled rectangle.
 		/// </summary>
 		/// <param name="pixels">The IPixels to draw on.</param>
