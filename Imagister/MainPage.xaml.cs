65b14f463ae65f9443546f122fa69861f9b3bd26
Steven Sheldon <steven.sheldon@live.com>
2012-04-19 08:17:07 +0000

    Test image loaded in C#.

--- Imagister/MainPage.xaml.cs
+++ Imagister/MainPage.xaml.cs
@@ -1,24 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
+using Microsoft.Phone;
 using Microsoft.Phone.Controls;
 
 namespace Imagister
 {
 	public partial class MainPage : PhoneApplicationPage
 	{
+		private ManipulableImage img;
+		private WriteableBitmap bmp;
+
 		// Constructor
 		public MainPage()
 		{
 			InitializeComponent();
+			LoadImage();
+		}
+
+		public void LoadImage()
+		{
+			Uri uri = new Uri("/Imagister;component/test.jpg", UriKind.Relative);
+			StreamResourceInfo sri = Application.GetResourceStream(uri);
+			bmp = PictureDecoder.DecodeJpeg(sri.Stream);
+			img = new ManipulableImage(bmp.PixelHeight, bmp.PixelWidth, bmp.Pixels);
+
+			imageControl.Source = bmp;
 		}
 	}
 }
