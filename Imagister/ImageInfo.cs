e0204643d4f26e3f7c95cb3e96a35a8c78f1564a
Steven Sheldon <steven.sheldon@live.com>
2012-04-20 03:12:28 +0000

    Added ImageInfo classes.

---
+++ Imagister/ImageInfo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Phone.Tasks;
+using Microsoft.Xna.Framework.Media;
+
+namespace Imagister
+{
+	public abstract class ImageInfo
+	{
+		public abstract Stream Stream
+		{
+			get;
+		}
+
+		public virtual ManipulableBitmap Bitmap
+		{
+			get { return new ManipulableBitmap(Stream); }
+		}
+	}
+
+	public class ImageUri : ImageInfo
+	{
+		private Uri uri;
+
+		public ImageUri(Uri uri)
+		{
+			this.uri = uri;
+		}
+
+		public override Stream Stream
+		{
+			get { return Application.GetResourceStream(uri).Stream; }
+		}
+	}
+
+	public class ImageResult : ImageInfo
+	{
+		private PhotoResult result;
+
+		public ImageResult(PhotoResult result)
+		{
+			this.result = result;
+		}
+
+		public override Stream Stream
+		{
+			get { return result.ChosenPhoto; }
+		}
+	}
+
+	public class ImageToken : ImageInfo
+	{
+		private string token;
+		private MediaLibrary lib;
+
+		public ImageToken(string token)
+		{
+			this.token = token;
+			this.lib = new MediaLibrary();
+		}
+
+		public Picture Picture
+		{
+			get { return lib.GetPictureFromToken(token); }
+		}
+
+		public override Stream Stream
+		{
+			get { return Picture.GetImage(); }
+		}
+	}
+}
